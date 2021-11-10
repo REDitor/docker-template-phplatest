c5a59f187687afec51d4c613946a5d6754fb16c8
sanderharks <650397@student.inholland.nl>
2021-11-10 15:41:07 +0100

    started

--- app/public/script.php
+++ app/public/script.php
@@ -1,13 +1,22 @@
 <html lang="en">
 <body>
 <?php
+$name = $birthdateParam = $age = "";
+
+if (empty($_POST["name"]))
+    echo "Name is required...";
+else
     $name = $_POST["name"];
+if (empty($_POST["dob"]))
+    echo "Select a birth date...";
+else {
     $birthdateParam = $_POST["dob"];
     $birthdate = new DateTime($birthdateParam);
     $now = new DateTime();
     $age = ($now->diff($birthdate))->y;
-    echo "Hello $name, you are $age years old";
+}
+
+//echo "Hello $name, you are $age years old";
 ?>
 </body>
 </html>
-
