875754ba7829f172dfe8d8ec74408166c122f26b
sanderharks <650397@student.inholland.nl>
2021-11-10 13:03:18 +0100

    first attempt

--- app/public/demo.php
+++ app/public/demo.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors');
+ini_set('display_startup_errors');
+error_reporting(E_ALL);
+
 $name = $_GET["name"];
 $birthdate = strtotime($_GET["birthdate"]);
 echo "Your name is $name and your birthday is $birthdate";
