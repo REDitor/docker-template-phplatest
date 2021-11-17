1eb8172cc9c0539a6ac28a6f29b8e8cc02881b2c
REDitor <71357600+REDitor@users.noreply.github.com>
2021-11-17 17:12:08 +0100

    doing

--- app/public/index.php
+++ app/public/index.php
@@ -21,27 +21,50 @@ $result = $connection->query($sql);
 //}
 //?>
 
-    <!doctype html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <style>
-            <?php include '../css/style.css'; ?>
-        </style>
-        <title>Guestbook</title>
-    </head>
-    <section class="container">
-    <h1>Guestbook</h1>
-<?php
-foreach ($result as $post) {
-    ?>
-    <section class="post">
-        <h2 class="post-name"><?php echo $post['name'] ?></h2>
-        <p><?php echo $post['message'] ?></p>
-        <p class="post-footer"><?php echo "posted at " . $post['posted_at'] . " from" . $post['ip_address'] ?></p>
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        <?php include '../css/style.css'; ?>
+    </style>
+    <title>Guestbook</title>
+</head>
+
+<body>
+<section id="main-container">
+    <section id="form-container">
+        <h1>Write something in our guestbook!</h1>
+        <form method="POST">
+            <fieldset class="form-field">
+                <label>Name:</label>
+                <input type="text" name="name">
+            </fieldset>
+            <fieldset class="form-field">
+                <label>Message:</label>
+                <textarea name="message"></textarea>
+            </fieldset>
+            <fieldset class="form-field">
+                <label>&nbsp;</label>
+                <input type="submit" value="Send">
+            </fieldset>
+        </form>
     </section>
-    <?php
-}?>
+
+    <section id="posts-container">
+        <h1>Guestbook</h1>
+        <?php
+        foreach ($result as $post) {
+            ?>
+            <section class="post">
+                <h2 class="post-name"><?php echo $post['name'] ?></h2>
+                <p><?php echo $post['message'] ?></p>
+                <p class="post-footer"><?php echo "posted at " . $post['posted_at'] . " from" . $post['ip_address'] ?></p>
+            </section>
+            <?php
+        } ?>
+    </section>
 </section>
+</body>
