695959bc520a9eed1428fe008dfcec8a0f005dbd
REDitor <71357600+REDitor@users.noreply.github.com>
2021-11-17 16:28:21 +0100

    finished and styled

--- app/public/index.php
+++ app/public/index.php
@@ -5,8 +5,8 @@ try {
     $connection = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //    echo "Connected successfully";
-} catch (PDOException $e){
-    echo "Connection failed: " .$e->getMessage();
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 
 $sql = "SELECT * FROM posts";
@@ -21,24 +21,27 @@ $result = $connection->query($sql);
 //}
 //?>
 
-<!doctype html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style
-    <title>Guestbook</title>
-</head>
-<h1>Guestbook</h1>
+    <!doctype html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <style>
+            <?php include '../css/style.css'; ?>
+        </style>
+        <title>Guestbook</title>
+    </head>
+    <section class="container">
+    <h1>Guestbook</h1>
 <?php
-include "../css/style.css";
-foreach ($result as $post){
-?>
-<section class="post">
-    <h2 class="post-name"><?php echo $post['name'] ?></h2>
-    <p><?php echo $post['message'] ?></p>
-    <p class="post-footer"><?php echo "posted at ".$post['posted_at']." from".$post['ip_address'] ?></p>
+foreach ($result as $post) {
+    ?>
+    <section class="post">
+        <h2 class="post-name"><?php echo $post['name'] ?></h2>
+        <p><?php echo $post['message'] ?></p>
+        <p class="post-footer"><?php echo "posted at " . $post['posted_at'] . " from" . $post['ip_address'] ?></p>
+    </section>
+    <?php
+}?>
 </section>
-<?php
-}
