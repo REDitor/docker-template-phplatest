7cfbf2c9b3bf816f288a2e735570b821dd41150b
REDitor <71357600+REDitor@users.noreply.github.com>
2021-12-04 12:43:26 +0100

    doing

--- app/public/script/editpost.php
+++ app/public/script/editpost.php
@@ -1,20 +1,24 @@
 <?php
 require_once ('../../dbconnection.php');
 
-$id = $_GET['id'];
-$name = $_GET['name'];
-$email = $_GET['email'];
-$message = $_GET['message'];
-$posted_at = $_GET['posted_at'];
-$ip_address = $_GET['ip_address'];
+
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $id = $_GET['id'];
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $message = $_GET['message'];
+    $posted_at = $_GET['posted_at'];
+    $ip_address = $_GET['ip_address'];
+
     $sql = "UPDATE posts
             SET message = :message
-            WHERE id = $id";
+            WHERE id = :id";
 
     if ($stmt = $connection->prepare($sql)) {
         $stmt->bindparam(':message', $message);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
         header("Location: ../management.php");
     }
 }
